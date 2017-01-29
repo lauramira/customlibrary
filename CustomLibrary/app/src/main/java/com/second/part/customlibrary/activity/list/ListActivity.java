fb8b1a79218a74697c497cc938a734a8b54c36ff
lmira <laura.mira.2016@salleurl.edu>
2017-01-29 17:21:15 +0100

    Added tab layout on list activity

--- CustomLibrary/app/src/main/java/com/second/part/customlibrary/activity/list/ListActivity.java
+++ CustomLibrary/app/src/main/java/com/second/part/customlibrary/activity/list/ListActivity.java
@@ -2,9 +2,9 @@ package com.second.part.customlibrary.activity.list;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.second.part.customlibrary.R;
 import com.second.part.customlibrary.activity.customactivity.CustomToolbarActivity;
@@ -13,6 +13,7 @@ import com.second.part.customlibrary.activity.list.recycler.Adapter;
 public class ListActivity extends CustomToolbarActivity {
 
     private Adapter adapter;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,19 +26,42 @@ public class ListActivity extends CustomToolbarActivity {
     }
 
     @Override
-    protected void customizeToolbar() {}
+    protected void customizeToolbar() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
     @Override
     protected void initViews() {
         super.initViews();
-       /* //FRAGMENT
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.activity_tab_layout_container);
-        ViewPager viewPager = (ViewPager) findViewById(R.id.activity_tab_layout_view_pager);*/
+        //TAB LAYOUT
+        tabLayout = (TabLayout) findViewById(R.id.activity_library_list_tab_layout);
+        initTabLayout();
 
         //RECYCLER
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.activity_library_list_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new Adapter(this);
         recyclerView.setAdapter(adapter);
+    }
+
+    private void initTabLayout(){
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.tab_layout_movies)));
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.tab_layout_films)));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 0){
+                    adapter.changeListType(1);
+                } else {
+                    adapter.changeListType(2);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
     }
 }
